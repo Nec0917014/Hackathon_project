87a6ac9866ca526a837634312485ba4f4c0bad20
vasanth <vasanthakumar.k@basiscloudsolutions.com>
2024-03-28 00:10:21 +0530

    product list added

--- fresh-farm/src/components/MainContainer.jsx
+++ fresh-farm/src/components/MainContainer.jsx
@@ -6,10 +6,77 @@ import RowContainer from "./RowContainer";
 import { useStateValue } from "../context/StateProvider";
 import MenuContainer from "./MenuContainer";
 import CartContainer from "./CartContainer";
+import Carrot from '../img/carrot.png'
 
 const MainContainer = () => {
   const [{ foodItems, cartShow }, dispatch] = useStateValue();
   const [scrollValue, setScrollValue] = useState(0);
+  const data= [
+    {
+      imageURL: Carrot,
+      title: 'Carrot',
+      calories: '20 callories',
+      category: 'fruits',
+      price: '100',
+      qty: 1
+    },
+    {
+      imageURL: Carrot,
+      title: 'Tomato',
+      calories: '20 callories',
+      category: 'fruits',
+      price: '200',
+      qty: 1
+
+    },
+    {
+      imageURL: Carrot,
+      title: 'Potato',
+      calories: '20 callories',
+      category: 'fruits',
+      price: '300',
+      qty: 1
+
+
+    },
+    {
+      imageURL: Carrot,
+      title: 'Onion',
+      calories: '20 callories',
+      category: 'fruits',
+      price: '400',
+      qty: 1
+
+
+    },
+    {
+      imageURL: Carrot,
+      title: 'Cauliflower',
+      calories: '20 callories',
+      category: 'fruits',
+      price: '500'
+
+    },
+    {
+      imageURL: Carrot,
+      title: 'Beetroot',
+      calories: '20 callories',
+      category: 'fruits',
+      price: '600',
+      qty: 1
+
+
+    },
+    {
+      imageURL: Carrot,
+      title: 'Cucumber',
+      calories: '20 callories',
+      category: 'fruits',
+      price: '700',
+      qty: 1
+
+    }
+  ]
 
   useEffect(() => {}, [scrollValue, cartShow]);
 
@@ -18,6 +85,36 @@ const MainContainer = () => {
       <HomeContainer />
 
       <section className="w-full my-6">
+        <div className="w-full flex items-center justify-between">
+          <p className="text-2xl font-semibold capitalize text-headingColor relative before:absolute before:rounded-lg before:content before:w-32 before:h-1 before:-bottom-2 before:left-0 before:bg-gradient-to-tr from-orange-400 to-orange-600 transition-all ease-in-out duration-100">
+            Our fresh & healthy vegetables
+          </p>
+
+          <div className="hidden md:flex gap-3 items-center">
+            <motion.div
+              whileTap={{ scale: 0.75 }}
+              className="w-8 h-8 rounded-lg bg-orange-300 hover:bg-orange-500 cursor-pointer  hover:shadow-lg flex items-center justify-center"
+              onClick={() => setScrollValue(-200)}
+            >
+              <MdChevronLeft className="text-lg text-white" />
+            </motion.div>
+            <motion.div
+              whileTap={{ scale: 0.75 }}
+              className="w-8 h-8 rounded-lg bg-orange-300 hover:bg-orange-500 cursor-pointer transition-all duration-100 ease-in-out hover:shadow-lg flex items-center justify-center"
+              onClick={() => setScrollValue(200)}
+            >
+              <MdChevronRight className="text-lg text-white" />
+            </motion.div>
+          </div>
+        </div>
+        <RowContainer
+          scrollValue={scrollValue}
+          flag={true}
+          data={data?.filter((n) => n.category === "fruits")}
+        />
+      </section>
+
+       <section className="w-full my-6">
         <div className="w-full flex items-center justify-between">
           <p className="text-2xl font-semibold capitalize text-headingColor relative before:absolute before:rounded-lg before:content before:w-32 before:h-1 before:-bottom-2 before:left-0 before:bg-gradient-to-tr from-orange-400 to-orange-600 transition-all ease-in-out duration-100">
             Our fresh & healthy fruits
@@ -43,7 +140,7 @@ const MainContainer = () => {
         <RowContainer
           scrollValue={scrollValue}
           flag={true}
-          data={foodItems?.filter((n) => n.category === "fruits")}
+          data={data?.filter((n) => n.category === "fruits")}
         />
       </section>
 
